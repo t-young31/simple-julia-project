c7bd32e526a138da3b6d294f9f00775effd18de9
tom <tom.young@chem.ox.ac.uk>
2022-04-15 09:12:01 +0100

    Typo

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,6 @@ using Documenter, InvertMatrix
 makedocs(modules = [InvertMatrix], sitename = "simple-julia-project")
 
 deploydocs(
-           repo = "github.com/t-young31/simple-julia-project.git"
+           repo = "github.com/t-young31/simple-julia-project.git",
            devbranch = "main"
 )
